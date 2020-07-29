146c911f4a0b98478712e98a0ada2629a5f4d5b6
amulla <kibrisli7@gmail.com>
2020-07-29 15:07:46 +0100

    Css styling for nav bar

--- madde/admin.py
+++ madde/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Maddeler
+
+
+class MaddelerAdmin(admin.ModelAdmin):
+    list_display = ('url', 'fiyat', 'kargo', 'kupon', 'baslik', 'ayrintilar', 'katagori', 'bas_tarih', 'son_tarih', 'online', 'mekan')
+    fields = ['url', 'fiyat', 'kargo', 'kupon', 'baslik', 'ayrintilar', 'katagori', 'bas_tarih', 'son_tarih', 'online', 'mekan']
+
+    list_filter = ('url', 'fiyat', 'kargo', 'kupon', 'baslik', 'ayrintilar', 'katagori', 'bas_tarih', 'son_tarih', 'online', 'mekan')
+
+
+admin.site.register(Maddeler, MaddelerAdmin)
--- madde/models.py
+++ madde/models.py
@@ -3,12 +3,13 @@ from django.db import models
 # Create your models here.
 class Maddeler(models.Model):
     url = models.URLField(max_length=200)
-    fiyat = models.DecimalField(decimal_places=2)
+    fiyat = models.DecimalField(max_digits=5, decimal_places=2)
     kargo = models.BooleanField()
     kupon = models.CharField(max_length=100)
     baslik = models.CharField(max_length=300)
     ayrintilar = models.TextField()
+    katagori = models.CharField(max_length=100)
     bas_tarih = models.DateField()
     son_tarih = models.DateField()
-    online = kargo = models.BooleanField()
+    online = models.BooleanField()
     mekan = models.CharField(max_length=100)
