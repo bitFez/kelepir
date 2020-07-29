8def67c9d7846dd449b7db1ffa8f2435088eaa36
amulla <kibrisli7@gmail.com>
2020-07-29 15:26:36 +0100

    added a users table

---
+++ hesaplar/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+class Kullanici(models.Model):
+    Kullanici = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    paylasimlar = models.IntegerField()
+    yorumlar = models.IntegerField()
+    ensicak = models.IntegerField()
+    takipciler = models.IntegerField()
+    
--- kelepirbul/settings.py
+++ kelepirbul/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'madde',
+    'hesaplar',
 ]
 
 MIDDLEWARE = [
