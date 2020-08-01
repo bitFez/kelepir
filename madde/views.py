aa2017ea5ad77f6a54d52900d0b345a5536d0e24
amulla <kibrisli7@gmail.com>
2020-08-01 18:47:59 +0100

    Completed functions for upvoting and downvoting

--- madde/views.py
+++ madde/views.py
@@ -8,8 +8,42 @@ def index(request):
     context = {'kelepirler':kelepirler}
     return render(request, 'madde/index.html', context)
 
-def voting():
-    pass
+
+@login_required
+def upvote(request,madde_id):
+    try:
+        vote = Vote.objects.get(madde=madde_id, kullanici=request.user)
+      except Vote.DoesNotExist:
+        vote = None
+
+      if vote is None:
+        # find madde by id and increment
+        kelepir = Maddeler.objects.get(id=madde)
+        vote = Vote(kelepir=kelepir, user=request.user)
+        kelepir.votes_total += 1
+        kelepir.derece +=2
+
+        vote.save()
+        kelepir.save()
+
+@login_required
+def downvote(request,madde_id):
+    try:
+        vote = Vote.objects.get(madde=madde_id, kullanici=request.user)
+      except Vote.DoesNotExist:
+        vote = None
+
+      if vote is None:
+        # find madde by id and increment
+        kelepir = Maddeler.objects.get(id=madde)
+        vote = Vote(kelepir=kelepir, user=request.user)
+        kelepir.votes_total += 1
+        kelepir.derece -=2
+
+        vote.save()
+        kelepir.save()
+
+
 
 class MaddeDetailView(generic.DetailView):
     model = Maddeler
