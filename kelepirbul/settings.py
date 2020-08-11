69fb9fc4b104f9bcabc846ee11de45d0f06aa26d
amulla <kibrisli7@gmail.com>
2020-08-11 12:13:38 +0100

    Added authenticate by email as well as username

--- kelepirbul/settings.py
+++ kelepirbul/settings.py
@@ -82,6 +82,7 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.twitter.TwitterOAuth',
     'social_core.backends.github.GithubOAuth2',
     'social_core.backends.google.GoogleOAuth2',
+    'hesaplar.authentication.EmailBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
 
