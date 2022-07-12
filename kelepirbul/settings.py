63a2557098d38b4f03de8167dcbcdcdc329af1fe
Mr Mulla <Your email>
2022-07-12 10:41:35 +0100

    security update

--- kelepirbul/settings.py
+++ kelepirbul/settings.py
@@ -1,17 +1,21 @@
 import os
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')2wv-%h3_0a=#k5u+-gy@e$)a*w##bn1nw(hut1lcqbe@gt#6w'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
